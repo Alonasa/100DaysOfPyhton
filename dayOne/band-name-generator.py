feaba07124c0b5e5e3168dd92a4757be1828cc40
Alonesa <alenakrasnov@mail.ru>
2023-10-26 16:33:14 +0100

    Band name generator

---
+++ dayOne/band-name-generator.py
@@ -0,0 +1,4 @@
+greeting = print('Welcome to the Band name Generator.')
+city = input("What's name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+print(f"Yor band name could be {city.capitalize()} {pet.capitalize()}")
