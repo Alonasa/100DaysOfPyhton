6f815a7532ddedee36965c7d58f69f71ce83e9ed
Alonesa <alenakrasnov@mail.ru>
2023-10-26 19:48:16 +0100

    change directory name for same codestyle

---
+++ dayOne/band_name_generator.py
@@ -0,0 +1,7 @@
+'''
+  Module generate Band name
+'''
+greeting = print('Welcome to the Band name Generator.')
+city = input("What's name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+print(f"Your band name could be {city.capitalize()} {pet.capitalize()}")
