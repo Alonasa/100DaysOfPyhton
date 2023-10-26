69086c4421a6a091b001852f08f6b19e0ff4bfa1
Alonesa <alenakrasnov@mail.ru>
2023-10-26 19:43:53 +0100

    fix filenames, added docstrings, make  lesson-2

---
+++ tipCalculator/tip_calculator.py
@@ -0,0 +1,10 @@
+'''
+  That module count tips
+'''
+
+print("Welcome to the tip calculator!")
+bill_amount = float(input("What was the total bill? "))
+amount_people = int(input("How many people to split the bill? "))
+tip_percent = int(input("What percentage tip would you like to give? "))
+to_pay = (bill_amount + bill_amount * (tip_percent/100))/amount_people
+print(f"Each person should pay: {to_pay}")
