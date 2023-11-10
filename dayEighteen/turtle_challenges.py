3eb96eea3daa06ffd5fae5855894aca87a9f42f4
Alonesa <alenakrasnov@mail.ru>
2023-11-10 17:44:24 +0000

    draw turtle square and dashed line

--- dayEighteen/turtle_challenges.py
+++ dayEighteen/turtle_challenges.py
@@ -3,11 +3,24 @@ from turtle import Turtle, Screen
 turtle_char = Turtle()
 turtle_char.shape("turtle")
 turtle_char.color("green")
-count = 0
 
-while count < 4:
-    count += 1
-    turtle_char.forward(100)
-    turtle_char.left(90)
+
+def square():
+    count = 0
+    while count < 4:
+        count += 1
+        turtle_char.forward(100)
+        turtle_char.left(90)
+
+
+def dashed():
+    count = 0
+    while count < 10:
+        count += 1
+        turtle_char.forward(10)
+        turtle_char.up()
+        turtle_char.forward(5)
+        turtle_char.down()
+
 
 screen = Screen()
