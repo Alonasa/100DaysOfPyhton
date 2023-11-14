ffe038cfa0231a75f80584a0598be08ed57b91ae
Alonesa <alenakrasnov@mail.ru>
2023-11-14 01:30:23 +0000

    set snake and dot

--- dayTwenty/shake.py
+++ dayTwenty/shake.py
@@ -1,9 +1,21 @@
 from turtle import Screen, Turtle
+import random
 
 screen = Screen()
-screen.setup(800, 800)
+screen.setup(1000, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+snake = Turtle()
+snake.shape("square")
+snake.shapesize(0.2, 0.8)
+snake.color("white")
+
+dot = Turtle()
+dot.shape("square")
+dot.shapesize(0.2, 0.2)
+dot.color("white")
+dot.penup()
+dot.goto(x=50.0, y=0.0)
+
 screen.exitonclick()
-score = 0
-screen.title(f"Score: {score}")
