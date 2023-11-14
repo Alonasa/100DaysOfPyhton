8995b0b2586e12bd19c6a08a2b6a8be01d39fe84
Alonesa <alenakrasnov@mail.ru>
2023-11-14 01:40:38 +0000

    make a dot redraw

--- dayTwenty/shake.py
+++ dayTwenty/shake.py
@@ -2,7 +2,9 @@ from turtle import Screen, Turtle
 import random
 
 screen = Screen()
-screen.setup(1000, 600)
+WIDTH = 1000
+HEIGHT = 600
+screen.setup(WIDTH, HEIGHT)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
@@ -17,5 +19,17 @@ dot.shapesize(0.2, 0.2)
 dot.color("white")
 dot.penup()
 dot.goto(x=50.0, y=0.0)
+
+
+def get_dot_coordinates():
+    pos_x = int(WIDTH / 2 - 5)
+    pos_y = int(HEIGHT / 2 - 5)
+    x = random.randint(-pos_x, pos_x)
+    y = random.randint(-pos_y, pos_y)
+
+    return x, y
+
+
+dot.goto(get_dot_coordinates())
 
 screen.exitonclick()
