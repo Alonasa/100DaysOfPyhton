f46226a15d0e91fd58faba0a18ae94ced2cd6330
Alonesa <alenakrasnov@mail.ru>
2023-11-14 01:03:18 +0000

    set screen

---
+++ dayTwenty/shake.py
@@ -0,0 +1,9 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(800, 800)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.exitonclick()
+score = 0
+screen.title(f"Score: {score}")
