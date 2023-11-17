ff18ce24f853ea900c85dcb7c87ba1b7d8055af3
Alonesa <alenakrasnov@mail.ru>
2023-11-17 01:21:47 +0000

    make paddle moving

--- dayTwentyTwo/paddle.py
+++ dayTwentyTwo/paddle.py
@@ -9,3 +9,10 @@ class Paddle(Turtle):
         self.shape("square")
         self.shapesize(5, 1)
         self.goto(width, 0)
+        self.w = width
+
+    def move_up(self):
+        self.goto(self.w, -150)
+
+    def move_down(self):
+        self.goto(self.w, 150)
--- dayTwentyTwo/pong.py
+++ dayTwentyTwo/pong.py
@@ -13,5 +13,10 @@ screen.title("Pong")
 dashed = MiddleLine(HEIGHT).draw_dashed()
 paddl1 = Paddle(WIDTH / 2 - 30)
 paddl2 = Paddle(-WIDTH / 2 + 30)
+current_paddle = paddl1
+
+screen.listen()
+screen.onkey(current_paddle.move_up, "Up")
+screen.onkey(current_paddle.move_down, "Down")
 
 screen.exitonclick()
