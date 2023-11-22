f29de7dc5a88a65e478faa7c513eeabae2488f1f
Alonesa <alenakrasnov@mail.ru>
2023-11-22 00:13:24 +0000

    make letters samples task

---
+++ dayTwentyFour/main.py
@@ -0,0 +1,18 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt") as text:
+    with open("Input/Names/invited_names.txt") as names:
+        for name in names:
+            name = name.strip()
+            with open(f"A:/100DAYSPYTHON/dayTwentyFour/Output/ReadyToSend/{name.strip()}.txt", "w") as to_send:
+                new_text = ''
+                for i in text:
+                    new_text += i.replace("[name],", name)
+                to_send.write(f"{new_text}")
