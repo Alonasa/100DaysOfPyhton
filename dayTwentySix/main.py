7914414aba6462dcd7151a53edff57c8e3192f1e
Alonesa <alenakrasnov@mail.ru>
2023-11-24 23:58:28 +0000

    make phonetic alphabet converter

--- dayTwentySix/main.py
+++ dayTwentySix/main.py
@@ -1,3 +1,11 @@
-ran = range(1, 20)
-range_list = [char * 2 for char in ran]
-print(range_list)
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+decoder = {code.letter: code.code for (idx, code) in data.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+get_name = input("Input your name: ").upper()
+encoder = [decoder[le] for le in get_name]
+print(encoder)
