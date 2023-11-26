c4f41df47aeab8a4a4ff042ec2c62ad2d904fa3d
Alonesa <alenakrasnov@mail.ru>
2023-11-26 00:32:31 +0000

    make unidirectional mode

--- dayTwentySeven/main.py
+++ dayTwentySeven/main.py
@@ -3,25 +3,44 @@ import tkinter
 window = tkinter.Tk()
 window.title("Mile to kilometers converter")
 window.minsize(width=500, height=400)
-
-label = tkinter.Label(text="Kilometers: ")
+radio = tkinter.StringVar()
+label = tkinter.Label(text="Convert to Kilometers")
 label.pack()
 
 MILES_DEFAULT = 1.60934
+KM_DEFAULT = 0.621371
+
+
+def check_selected():
+    selected_option = radio.get()
+    if selected_option == "Km":
+        return "Km"
+    else:
+        return "Mi"
 
 
 def button_clicked():
-    print("I got click")
-    text = float(input.get())
-    convert = text * MILES_DEFAULT
-    label.config(text=f"In a {text} Mile{'s' if text > 1 else ''} {convert} Kilometers")
+    text = float(inp.get())
+    convert_tokm = text * MILES_DEFAULT
+    convert_tomi = text * KM_DEFAULT
+    selected = check_selected()
+    if selected == "Km":
+        label.config(text=f"In a {text} Mile{'s' if text > 1 else ''} {convert_tokm} Kilometers")
+    else:
+        label.config(text=f"In a {text} Kilometer{'s' if text > 1 else ''} {convert_tomi} Miles")
 
+
+ki_radio = tkinter.Radiobutton(text="Km", variable=radio, value="Km")
+mi_radio = tkinter.Radiobutton(text="Mi", variable=radio, value="Mi")
+ki_radio.pack()
+mi_radio.pack()
 
 button = tkinter.Button(text="Convert", command=button_clicked)
 button.pack()
 
-input = tkinter.Entry(width=50)
-input.pack()
-input.get()
+inp = tkinter.Entry(width=50)
+inp.focus()
+inp.pack()
+inp.get()
 
 window.mainloop()
