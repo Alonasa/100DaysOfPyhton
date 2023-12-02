f3a916b97c53345179003133b4bc48b14d0ce492
Alonesa <alenakrasnov@mail.ru>
2023-12-02 23:12:18 +0000

    added all UI

--- dayThirtyOne/main.py
+++ dayThirtyOne/main.py
@@ -1,45 +1,25 @@
-from tkinter import Canvas, Tk
+from tkinter import Canvas, Tk, Button, PhotoImage
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
+font = ("Ariel", 40, "italic")
 
 window.title("Language Cards")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
-canvas = Canvas(width=700, height=526, highlightthickness=0, bd=0, background=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526, highlightthickness=0, bd=0, background=BACKGROUND_COLOR)
 
-canvas.grid(row=0, column=1)
+card_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+canvas.create_image(400, 263, anchor="center", image=card_img)
+canvas.grid(row=0, column=1, columnspan=2, rowspan=2)
+canvas.create_text(400, 150, text="Title", font=font)
+canvas.create_text(400, 250, text="Description", font=(font, 50, "bold"))
 
-
-def create_rounded_rectangle(figure, x1, y1, x2, y2, radius, **kwargs):
-    # Calculate coordinates for rounded rectangle
-    points = [
-        x1 + radius, y1,
-        x1 + radius, y1,
-        x2 - radius, y1,
-        x2 - radius, y1,
-        x2, y1,
-        x2, y1 + radius,
-        x2, y1 + radius,
-        x2, y2 - radius,
-        x2, y2 - radius,
-        x2, y2,
-        x2 - radius, y2,
-        x2 - radius, y2,
-        x1 + radius, y2,
-        x1 + radius, y2,
-        x1, y2,
-        x1, y2 - radius,
-        x1, y2 - radius,
-        x1, y1 + radius,
-        x1, y1 + radius,
-        x1, y1
-    ]
-
-    # Draw rounded rectangle on canvas
-    return figure.create_polygon(points, **kwargs, smooth=True)
-
-
-rounded_rectangle = create_rounded_rectangle(canvas, 0, 0, 700, 526, radius=25, fill="white", outline="")
-
+right_img = PhotoImage(file="images/right.png")
+left_img = PhotoImage(file="images/wrong.png")
+button_right = Button(image=right_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0)
+button_right.grid(row=3, column=1)
+button_left = Button(image=left_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0)
+button_left.grid(row=3, column=2)
 window.mainloop()
