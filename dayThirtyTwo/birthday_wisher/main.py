c41dfae60eb0e7736ffce993538bca9a8c140971
Alonesa <alenakrasnov@mail.ru>
2023-12-03 23:53:23 +0000

    change data from the initial letter for one from template

--- dayThirtyTwo/birthday_wisher/main.py
+++ dayThirtyTwo/birthday_wisher/main.py
@@ -15,11 +15,16 @@ folder_path = "letter_templates"  # Replace with the actual folder path
 # Get a list of all files in the folder
 file_list = os.listdir(folder_path)
 greeting = random.choice(file_list)
-
 current_month = dt.datetime.now().month
 current_day = dt.datetime.now().day
-print(birthday)
+
 for el in birthday:
     if el["month"] == current_month and el["day"] == current_day:
-        print(f"Its a birthday of {el['name']}")
-        print()
+        content = f"{folder_path}/{greeting}"
+        with open(content, "r") as text:
+            letter = text.read()
+            old_entries = ["[NAME]", "Angela"]
+            new_entries = [el["name"], "Alona"]
+            new_text = letter
+            for old_entry, new_entry in zip(old_entries, new_entries):
+                new_text = new_text.replace(old_entry, new_entry)
