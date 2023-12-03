39adabc33934ee393c43f3c8d559c2780172c226
Alonesa <alenakrasnov@mail.ru>
2023-12-03 00:13:49 +0000

    make words and lang change according to pressed button

--- dayThirtyOne/main.py
+++ dayThirtyOne/main.py
@@ -1,7 +1,30 @@
+import random
 from tkinter import Canvas, Tk, Button, PhotoImage
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/french_words.csv")
+words = data.to_dict(orient="records")
+
+item = {}
+
+
+def change_word():
+    global item
+    item = random.choice(words)
+    canvas.itemconfig(card_language, text="French")
+    canvas.itemconfig(card_word, text=item["French"])
+    return item
+
+
+def learn_word():
+    global item
+    canvas.itemconfig(card_language, text="English")
+    canvas.itemconfig(card_word, text=item["English"])
+
+
+# ----------------------- UI------------------------------------------
 window = Tk()
 font = ("Ariel", 40, "italic")
 
@@ -13,13 +36,14 @@ card_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 canvas.create_image(400, 263, anchor="center", image=card_img)
 canvas.grid(row=0, column=1, columnspan=2, rowspan=2)
-canvas.create_text(400, 150, text="Title", font=font)
-canvas.create_text(400, 250, text="Description", font=(font, 50, "bold"))
+card_language = canvas.create_text(400, 150, text="title", font=font)
+card_word = canvas.create_text(400, 250, text="Description", font=(font, 50, "bold"))
 
 right_img = PhotoImage(file="images/right.png")
 left_img = PhotoImage(file="images/wrong.png")
-button_right = Button(image=right_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0)
+button_right = Button(image=right_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0, command=change_word)
 button_right.grid(row=3, column=1)
-button_left = Button(image=left_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0)
+button_left = Button(image=left_img, background=BACKGROUND_COLOR, highlightthickness=0, bd=0, command=learn_word)
 button_left.grid(row=3, column=2)
+
 window.mainloop()
