9b25ebded796ca7120ddca21814e3c40adeb86a7
Alonesa <alenakrasnov@mail.ru>
2023-12-03 23:19:18 +0000

    get random letter from the list of greeting and find the person with a birthday

--- dayThirtyTwo/birthday_wisher/main.py
+++ dayThirtyTwo/birthday_wisher/main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import smtplib
 import datetime as dt
@@ -9,9 +10,16 @@ email = "all.junk.mails.my@gmail.com"
 
 birthday = pandas.read_csv("birthdays.csv").to_dict(orient="records")
 
+folder_path = "letter_templates"  # Replace with the actual folder path
+
+# Get a list of all files in the folder
+file_list = os.listdir(folder_path)
+greeting = random.choice(file_list)
+
 current_month = dt.datetime.now().month
 current_day = dt.datetime.now().day
 print(birthday)
 for el in birthday:
     if el["month"] == current_month and el["day"] == current_day:
         print(f"Its a birthday of {el['name']}")
+        print()
