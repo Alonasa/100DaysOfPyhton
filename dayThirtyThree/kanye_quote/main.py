a7da0ad36184ca507a8dd75962ca2261bedc239e
Alonesa <alenakrasnov@mail.ru>
2023-12-05 21:43:27 +0000

    fix quote overflow

--- dayThirtyThree/kanye_quote/main.py
+++ dayThirtyThree/kanye_quote/main.py
@@ -5,9 +5,10 @@ import requests
 def get_quote():
     response = requests.get("https://api.kanye.rest/")
     quote = response.json()["quote"]
-    canvas.itemconfig(quote_text, text=quote)
-    print(len(quote))
-    return quote
+    if len(quote) < 90:
+        canvas.itemconfig(quote_text, text=quote)
+    else:
+        return get_quote()
 
 
 window = Tk()
@@ -17,7 +18,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
+quote_text = canvas.create_text(150, 200, text="Kanye Quote Goes HERE", width=285, font=("Arial", 30, "bold"),
                                 fill="white")
 canvas.grid(row=0, column=0)
 
