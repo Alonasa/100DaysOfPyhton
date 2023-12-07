eda998cd8f379b873bcaa109ae983cde2954fc68
Alonesa <alenakrasnov@mail.ru>
2023-12-07 23:56:23 +0000

    set main tkinter window

--- dayThirtyFour/quizer/ui.py
+++ dayThirtyFour/quizer/ui.py
@@ -1,2 +1,11 @@
+from tkinter import Tk, Canvas
+
 THEME_COLOR = "#375362"
 
+window = Tk()
+window.title("Quizer")
+
+canvas = Canvas(width=350, height=450)
+canvas.grid(row=0, column=1)
+
+window.mainloop()
