7e5330b7d2014af83d7c7a749126274dfcd0464d
Alonesa <alenakrasnov@mail.ru>
2023-12-09 01:44:10 +0000

    finish quiz game

--- dayThirtyFour/quizer/ui.py
+++ dayThirtyFour/quizer/ui.py
@@ -13,10 +13,11 @@ class QuizInterface:
         self.window.title("Quizer")
         self.window.config(width=350, height=450, padx=20, pady=20, background=THEME_COLOR)
 
-        self.score = Label(text="Score: 0", font=FONT, fg="white", background=THEME_COLOR)
+        self.score = Label(text=f"Score: 0", font=FONT, fg="white",
+                           background=THEME_COLOR)
         self.score.grid(row=0, column=1)
 
-        self.canvas = Canvas(width=300, height=250)
+        self.canvas = Canvas(width=300, height=250, background="white")
         self.question = self.canvas.create_text(150, 125, width=280, text="Text", font=FONT)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=40)
 
@@ -32,13 +33,24 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        question_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=question_text.capitalize())
+        if self.quiz.still_has_questions():
+            self.canvas.config(background="white")
+            self.score.config(text=f"{self.quiz.score} / {self.quiz.question_number}")
+            question_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=question_text.capitalize())
+        else:
+            self.canvas.itemconfig(self.question, text="You're reach the end of the Quiz")
+            self.canvas.config(background="white")
+            self.button_yes.config(state="disabled")
+            self.button_no.config(state="disabled")
 
     def answer_true(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        self.answer_feedback(self.quiz.check_answer("True"))
 
     def answer_false(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        self.answer_feedback(self.quiz.check_answer("False"))
+
+    def answer_feedback(self, is_right):
+        bg = "green" if is_right else "red"
+        self.canvas.config(background=bg, highlightthickness=0, bd=0)
+        self.window.after(1000, self.get_next_question)
