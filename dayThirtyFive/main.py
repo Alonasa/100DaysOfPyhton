a93fefd32cbc1c07a33518757d452083f366510f
Alonesa <alenakrasnov@mail.ru>
2023-12-10 00:20:29 +0000

    make weather sended by email

--- dayThirtyFive/main.py
+++ dayThirtyFive/main.py
@@ -1,4 +1,6 @@
+import smtplib
 from datetime import datetime, timedelta
+from email.message import EmailMessage
 
 import requests
 
@@ -6,6 +8,10 @@ endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "6419269eef1e3c2969a8eb8172cacbb9"
 lat = 53.43333
 lon = -7.95
+
+my_email = "all.junk.mails.my@gmail.com"
+password = "jjbw dcrj tzun uydj"
+email = "all.junk.mails.my@gmail.com"
 
 parameters = {
     "lat": lat,
@@ -16,11 +22,12 @@ parameters = {
 
 general_data = requests.get(url=endpoint, params=parameters)
 data = general_data.json()["list"]
-date_today = datetime.today().date() + timedelta(days=1)
+date_today = datetime.today().date()
 for item in data:
     day = item["dt_txt"].split(" ")
     weather = item["weather"][0]["description"].capitalize()
     if str(day[0]) == str(date_today):
+        current_weather = f"We expect {weather} in between {day[1]}"
         if item["weather"][0]["id"] < 700:
             datetime_obj = datetime.strptime(day[1], "%H:%M:%S")
 
@@ -32,6 +39,14 @@ for item in data:
 
             # Convert time object back to string
             new_time_str = new_time_obj.strftime("%H:%M:%S")
+            rain_notice = f"We expect {weather} in between {day[1]}-{new_time_obj}"
+            print(rain_notice)
 
-            print(
-                f"We expect {weather} in between {day[1]}-{new_time_obj}")
+            msg = EmailMessage()
+            msg["Subject"] = "Rain notification"
+            msg.set_content(rain_notice)
+
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.send_message(msg=msg, from_addr=my_email, to_addrs=email)
