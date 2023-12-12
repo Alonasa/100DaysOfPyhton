abbfb231ca103cb9caecb30e60120ea1ab523440
Alonesa <alenakrasnov@mail.ru>
2023-12-12 22:13:33 +0000

    get last 10 news articles if stock active gain or loose over 5% and print details about it

--- dayThirtySix/main.py
+++ dayThirtySix/main.py
@@ -6,26 +6,29 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 API_KEY = "7YMLK7K2SSOFF20T"
+NEWS_API_KEY = "f5d2e398a5c64dfba2db6a92db9a3f88"
+STOCK_ACTIVE = "IBM"
 
-parameters = {
+investment_parameters = {
     "function": "TIME_SERIES_DAILY",
-    "symbol": "IBM",
+    "symbol": STOCK_ACTIVE,
     "apikey": API_KEY,
     "datatype": "json"
 }
 
-request = requests.get(url=STOCK_ENDPOINT, params=parameters)
-data = request.json()['Time Series (Daily)']
-data_list = [value for key, value in data.items()]
+news_parameters = {
+    "q": STOCK_ACTIVE,
+    "apiKey": NEWS_API_KEY
+}
+
+get_investment = requests.get(url=STOCK_ENDPOINT, params=investment_parameters)
+investment_data = get_investment.json()['Time Series (Daily)']
+data_list = [value for key, value in investment_data.items()]
 yesterday_price = float(data_list[0]['4. close'])
 day_before_price = float(data_list[1]['4. close'])
 
-print(data_list)
 price_difference = yesterday_price - day_before_price
 get_five_percent = yesterday_price / 100 * 5
-
-if abs(price_difference) > get_five_percent:
-    print("difference is too high")
 
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -37,6 +40,15 @@ if abs(price_difference) > get_five_percent:
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
 # HINT 1: Think about using the Python Slice Operator
 
+if abs(price_difference) > get_five_percent:
+    get_news = requests.get(url=NEWS_ENDPOINT, params=news_parameters)
+    news_data = get_news.json()['articles']
+    for i in range(10):
+        news_article = news_data[i]
+        print(news_article['title'])
+        print(news_article['description'])
+        print(news_article['publishedAt'])
+        print(f"Learn {news_article['url']}")
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
