28cdbfaed29ec9c9cb92727c6d3a320626ea6d84
Alonesa <alenakrasnov@mail.ru>
2023-12-14 00:37:36 +0000

    make progress updating

--- dayThirtySeven/main.py
+++ dayThirtySeven/main.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 import requests
 
 USERNAME = "alona"
 TOKEN = "alonecoder25"
+GRAPH_ID = "graph"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
@@ -13,10 +16,10 @@ params = {
     "notMinor": "yes",
 }
 graph_params = {
-    "id": "graph",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "Tasks",
-    "type": "float",
+    "type": "int",
     "color": "kuro"
 }
 
@@ -24,5 +27,21 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
-print(response.text)
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+# today = datetime(year=2023, month=12, day=13)
+today = datetime.now()
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "5",
+}
+
+create_response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+
+pixel_update = {
+    "quantity": "5"
+}
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+update_response = requests.put(url=update_endpoint, json=pixel_update, headers=headers)
+print(update_response.text)
