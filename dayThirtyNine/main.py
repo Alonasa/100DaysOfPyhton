1237dfcda5ad068cbaf9da3062bf9c40b2a6f23f
Alonesa <alenakrasnov@mail.ru>
2023-12-17 02:47:05 +0000

    get iata codes for cities from our list and insert them in our spredshit

--- dayThirtyNine/main.py
+++ dayThirtyNine/main.py
@@ -1,23 +1,29 @@
 import requests
 from pprint import pprint
-import json
-from io import StringIO
 
 BASE_URL = "https://api.sheety.co/1188f4ef2e5889f0a49a02ef057290e9/flightDeals/prices"
 
 get_data = requests.get(url=BASE_URL)
 
 data = get_data.json()
-for item in data['prices']:
-    if item['iataCode'] == '':
-        itemId = item['id']
 
-        body = {
-            "price": {"iataCode": "TESTING"}
-        }
+for item in data["prices"]:
+    TEQUILA_LOC = f"https://api.tequila.kiwi.com/locations/query?term={item['city']}&locale=en-US&location_types=city&limit=10&active_only=true"
+    par = {
+        'apikey': 'qi0gKdipdlEG0zFW0Wh5KtiicSpgP_GV'
+    }
+    get_locations = requests.get(url=TEQUILA_LOC, headers=par)
+    locations_data = get_locations.json()
+    for location in locations_data["locations"]:
+        if item["iataCode"] == "TESTING":
+            if location["name"] == item["city"]:
+                iataCode = location["code"]
+                itemId = item["id"]
+                body = {
+                    "price": {"iataCode": iataCode}
+                }
+                header = {
+                    "Content-Type": "application/json"
+                }
 
-        header = {
-            "Content-Type": "application/json"
-        }
-
-        sent = requests.put(url=f"{BASE_URL}/{itemId}", json=body, headers=header)
+                sent = requests.put(url=f"{BASE_URL}/{itemId}", json=body, headers=header)
