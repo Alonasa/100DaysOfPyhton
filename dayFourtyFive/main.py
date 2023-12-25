b83bfc3f0c0f7e835f1b03aff7a7b4626e281b70
Alonesa <alenakrasnov@mail.ru>
2023-12-25 20:11:07 +0000

    scrap basic post information

--- dayFourtyFive/main.py
+++ dayFourtyFive/main.py
@@ -6,6 +6,17 @@ response = requests.get("https://dev.to/t/news")
 page = response.text
 
 content = BeautifulSoup(page, "html.parser")
-element = content.find(name="div", class_="crayons-story__indention")
-print(element)
+elements = content.find_all(name="div", class_="crayons-story__body")
+authors = (content.find_all(name="a", class_="crayons-story__secondary"))
+# .getText().strip())
+posting_dates = content.find_all(name="time")
+titles = content.find_all(name="h2", class_="crayons-story__title")
+hashtags = content.find_all(name="a", class_="crayons-tag")
+for hashtag in hashtags:
+    text = hashtag.getText()
+    print(text)
+
+for el in posting_dates:
+    print(el['datetime'])
+# print(element)
 # soup = BeautifulSoup(content, "lxml")
