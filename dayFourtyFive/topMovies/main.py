0c252ba7732e20e515dc3f345cf76a80b767f60a
Alonesa <alenakrasnov@mail.ru>
2023-12-26 20:05:25 +0000

    create the list of movies from scrapped website

--- dayFourtyFive/topMovies/main.py
+++ dayFourtyFive/topMovies/main.py
@@ -7,5 +7,9 @@ page = response.text
 movie_title = "listicleItem_listicle-item__title__BfenH"
 
 elements = BeautifulSoup(page, "html.parser")
-all_titles = elements.find_all(name="h3", class_=movie_title)
-print(all_titles)
+reversed_titles = elements.find_all(name="h3", class_=movie_title)
+titles = reversed_titles[::-1]
+with open("movies_to_watch.txt", "w", encoding="utf-8") as file:
+    for movie in titles:
+        movie = movie.getText()
+        file.write(f"{movie}\n")
