7f52cb1742ad1ce992c93de9b29436d78471d60c
Alonesa <alenakrasnov@mail.ru>
2024-01-01 20:01:48 +0000

    generated connection to spotify

--- dayFourtySix/main.py
+++ dayFourtySix/main.py
@@ -4,6 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 from requests import request
 
+client_id = "af95b8b6e51245d3bbde7942c54d3ffd"
+client_secret = "c9b1660a8386475cbda88586a4c5998e"
+
 base_url = "https://www.billboard.com/charts/hot-100"
 
 date = input("Write date to check music chart in format YYYY-MM-DD ").strip()
@@ -15,9 +18,7 @@ elements = BeautifulSoup(page, "html.parser")
 
 song_title = elements.select("li ul li h3")
 song_titles_list = [song.getText().strip() for song in song_title]
-song_author = elements.select("li ul li span")
-song_authors_list = [song.getText().strip() for song in song_author]
-print(song_authors_list)
+print(song_titles_list)
 
 # song_author = elements.find_all(name="span", class_="c-label")
 # songs = [song.getText() for song in song_titles]
