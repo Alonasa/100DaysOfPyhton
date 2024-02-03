2c6e2e4454e97bb2dbb4e05b55849c7ccbec5fe1
Alonesa <alenakrasnov@mail.ru>
2024-02-03 23:54:25 +0000

    created a new form, get link from it

---
+++ data_entry.py
@@ -0,0 +1,1 @@
+FORM_LINK = "https://forms.gle/5SzSdbMkdXPfMgQ66"
---
+++ dayFourtyNine/cookie_click.py
@@ -0,0 +1,9 @@
+# <div id="shimmers">
+# <div class="shimmer" alt="Золотое печенье"
+# style="left: 55px; top: 272px; width: 96px;
+# height: 96px; background-image: url(&quot;//cdn.dashnet.org/cookieclicker/img/goldCookie.png&quot;);
+# background-position: 0px 0px; opacity: 0.998838; display: block; transform: rotate(-8.51665deg) scale(1.11582);">
+# </div>
+
+# this.goldInterval = setInterval(function(){var shimer = document.getElementsByClassName("shimmer")[0]; if(shimer) {shimer.click()}})
+# </div>
