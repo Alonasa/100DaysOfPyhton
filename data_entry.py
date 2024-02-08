d4864535502b144281ce32405ff2a4ca2fbe172c
Alonesa <alenakrasnov@mail.ru>
2024-02-08 22:12:51 +0000

    get advertising links

--- data_entry.py
+++ data_entry.py
@@ -7,4 +7,6 @@ WEBSITE_LINK = "https://appbrewery.github.io/Zillow-Clone/"
 data = requests.get(url=WEBSITE_LINK).text
 elements = BeautifulSoup(data, "html.parser")
 
-ADVERTISING_LINKS = elements.find_all(attrs={"class": "StyledPropertyCardPhotoBody"})
+ADVERTISINGS = elements.find_all(attrs={"class": "StyledPropertyCardPhotoBody"})
+LINKS = [item.a["href"] for item in ADVERTISINGS]
+print(LINKS)
