93067e56c936de08e0755aaac8880ce7acfd3c82
Alonesa <alenakrasnov@mail.ru>
2024-02-08 23:44:52 +0000

    fix file structure and addresses

--- dayFiftyThree/data_entry.py
+++ dayFiftyThree/data_entry.py
@@ -11,7 +11,7 @@ ADVERTISINGS = elements.find_all(attrs={"class": "ListItem-c11n-8-84-3-StyledLis
 LINKS = [item.a["href"] for item in ADVERTISINGS]
 PRICES = [el.text.replace("+", "/").split("/")[0] for el in
           elements.find_all("span", {"data-test": "property-card-price"})]
-ADDRESSES = [[el.text.strip()] for el in elements.find_all("address")]
+ADDRESSES = [el.text.replace("|", ",").split(",", 1)[1].strip() for el in elements.find_all("address")]
 print(PRICES)
 print(LINKS)
 print(ADDRESSES)
