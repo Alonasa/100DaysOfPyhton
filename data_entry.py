1bd9fd3493a1756c60e3985c9ef396b4db605da2
Alonesa <alenakrasnov@mail.ru>
2024-02-08 23:21:37 +0000

    get prices

--- data_entry.py
+++ data_entry.py
@@ -7,6 +7,9 @@ WEBSITE_LINK = "https://appbrewery.github.io/Zillow-Clone/"
 data = requests.get(url=WEBSITE_LINK).text
 elements = BeautifulSoup(data, "html.parser")
 
-ADVERTISINGS = elements.find_all(attrs={"class": "StyledPropertyCardPhotoBody"})
+ADVERTISINGS = elements.find_all(attrs={"class": "ListItem-c11n-8-84-3-StyledListCardWrapper"})
 LINKS = [item.a["href"] for item in ADVERTISINGS]
+PRICES = [el.text.replace("+", "/").split("/")[0] for el in
+          elements.find_all("span", {"data-test": "property-card-price"})]
+print(PRICES)
 print(LINKS)
