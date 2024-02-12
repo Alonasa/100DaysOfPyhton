c460ff13953573f52704b664f00f5d6b514db10f
Alonesa <alenakrasnov@mail.ru>
2024-02-12 23:37:45 +0000

    make own decorator functions for routing

--- dayFiftyFour/main.py
+++ dayFiftyFour/main.py
@@ -1,3 +1,6 @@
+import time
+from functools import wraps
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +9,33 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>My first server</h1>'
+
+
+def person_function(func):
+    @wraps(func)
+    def person_decorator(*args, **kwargs):
+        print('I am a robot')
+        time.sleep(2)
+        return func(*args, **kwargs)
+
+    return person_decorator
+
+
+@app.route('/bot')
+@person_function
+def bot():
+    message = 'I am a bot'
+    print(message)
+    return message
+
+
+@app.route('/yo')
+@person_function
+def person_skills():
+    message = 'I know Python, JS, HTML, CSS'
+    print(message)
+    return message
+
+
+if __name__ == "__main__":
+    app.run()
