31e8019f2678cc9bf31fe8e460028e189b3a5342
Alonesa <alenakrasnov@mail.ru>
2024-03-05 23:33:16 +0000

    added basic template

--- dayFiftySeven/server.py
+++ dayFiftySeven/server.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+year = datetime.now().year
+
+
+def get_name():
+    if request.method == "POST":
+        name = request.form.get('name')
+        return name
+
+
+def get_click():
+    return 'Get click on button'
 
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("index.html", year=year)
+
+
+@app.route("/<username>")
+def show_data(username):
+    return render_template("index.html", name=username, year=year)
 
 
 if __name__ == "__main__":
