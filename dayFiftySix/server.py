e55f159d4d579f038d42e1386b7d43cd2f1a7a1f
Alonesa <alenakrasnov@mail.ru>
2024-03-07 15:20:35 +0000

    make form send requests to the owner of website

--- dayFiftySix/server.py
+++ dayFiftySix/server.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from flask import Flask, render_template, request
 
@@ -25,13 +27,23 @@ def send_form():
         name = request.form["contact-form__name"].strip()
         mail = request.form["contact-form__email"].strip()
         message = request.form["contact-form__message"].strip()
+        message_content = f"You just received new request from {mail} message - {message}"
+
+        msg = MIMEMultipart()
+        msg["From"] = mail
+        msg["To"] = my_email
+        msg["Subject"] = f"You received new request from {name}"
+
+        # Create the message content part
+        text_part = MIMEText(message_content, "plain")
+
+        # Attach the message content part to the email
+        msg.attach(text_part)
 
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=my_email, password=password)
-            connection.sendmail(from_addr=my_email, to_addrs=my_email,
-                                msg=f"Subject: You received new message from {mail}, "
-                                    f"Content: Name - {name} message - {message}")
+            connection.send_message(msg)
 
         return '<h1>Successfully sent your message</h1>'
 
