4b9b3d36ee680849991648ca866d911b9ea521c7
Alonesa <alenakrasnov@mail.ru>
2024-03-11 21:47:28 +0000

    created new database and make elements add to it

--- daySixtyThree/main.py
+++ daySixtyThree/main.py
@@ -1,13 +1,36 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from sqlalchemy import Integer, String, Float, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired, NumberRange
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(64)
 Bootstrap5(app)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 all_books = []
 
@@ -17,12 +40,21 @@ class AddBookForm(FlaskForm):
         "placeholder": "Book name e.g. Harry Potter"})
     author = StringField('Book Author', validators=[DataRequired()], render_kw={
         "placeholder": "Author name e.g. J. K. Rowling"})
-    rating = IntegerField('Rating', validators=[DataRequired(), NumberRange(min=1, max=10)], render_kw={
+    rating = FloatField('Rating', validators=[DataRequired(), NumberRange(min=1, max=10)], render_kw={
         "placeholder": "Add your rating from 1 to 10"})
     submit = SubmitField('Add book')
 
 
-# min=0, max=10, step=1,
+def get_last_db_id():
+    engine = create_engine('sqlite:///books-collection.db')  # Replace with your SQLite database file path
+    session = sessionmaker(bind=engine)
+    session = session()
+    last_id = session.query(Book.id).order_by(Book.id.desc()).first()
+
+    last_inserted_id = last_id[0] if last_id else None
+    return last_inserted_id
+
+
 @app.route('/')
 def home():
     if len(all_books) > 0:
@@ -43,6 +75,13 @@ def add():
                 'rating': f"{request.form['rating']}"
             }
             all_books.append(book)
+            last_id = get_last_db_id()
+
+            with app.app_context():
+                new_book = Book(id=int(last_id) + 1, title=book['title'], author=book['author'], rating=book[
+                    'rating'])
+                db.session.add(new_book)
+                db.session.commit()
 
             return redirect(url_for('home'))
     return render_template('add.html', form=form)
