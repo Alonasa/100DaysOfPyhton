1bbea3b9eb81084c507e74efe4db739d064fe4f9
Alonesa <alenakrasnov@mail.ru>
2024-03-18 22:39:39 +0000

    make menu show when user is logged in

--- daySixtyEight/main.py
+++ daySixtyEight/main.py
@@ -106,7 +106,7 @@ def login():
 @app.route("/secrets")
 @login_required
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route("/logout")
