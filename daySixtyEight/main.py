dcd9bd09a3a95f707faf27fedd764a7e752d5180
Alonesa <alenakrasnov@mail.ru>
2024-03-18 21:57:28 +0000

    added authorisation via werkzeug

--- daySixtyEight/main.py
+++ daySixtyEight/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +8,9 @@ from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret-key-goes-here"
+app.config["SECRET_KEY"] = os.urandom(36)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # CREATE DATABASE
@@ -20,11 +24,16 @@ db.init_app(app)
 
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
+
+    def __init__(self, email, password, name):
+        self.email = email
+        self.password = password
+        self.name = name
 
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
@@ -32,6 +41,11 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
 @app.route("/")
@@ -49,6 +63,8 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
+
         flash("New User Is Added")
         return redirect(url_for("login"))
 
@@ -57,52 +73,43 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    data = db.session.execute(db.select(User))
-    data_list = data.scalars().all()
-
     form_password = request.form.get("password")
     form_email = request.form.get("email")
+    data = db.session.execute(db.select(User))
+    data_list = data.scalars().all()
 
     if len(data_list) == 0:
         return render_template("register.html")
 
+    user = db.session.execute(db.select(User).where(User.email == form_email))
+    data = user.scalar()
+
     if form_email is None and form_password is None:
         return render_template("login.html")
 
-    for item in data_list:
-        password = check_password_hash(item.password, form_password)
-        email = item.email
+    if check_password_hash(data.password, form_password):
+        login_user(data, remember=True)
+        return redirect(url_for("secrets"))
+    else:
+        flash("Wrong fields. Please check your email and password")
 
-        if email == form_email and password:
-            session["authorized"] = True
-            session["username"] = item.name.capitalize()
-            return render_template("secrets.html", name=item.name.capitalize())
-        else:
-            flash("Wrong fields. Please check your email and password")
-
-    flash("Wrong credentials. Please check your email and password")
     return render_template("login.html")
 
 
 @app.route("/secrets")
+@login_required
 def secrets():
-    authorized = session.get("authorized")
-    user = session.get("username")
-
-    if authorized:
-        return render_template("secrets.html", name=user)
-    else:
-        flash("You dont have a permission to see this page!")
-        return redirect(url_for("login"))
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route("/logout")
 def logout():
-    session.pop("authorized", None)
-    return redirect(url_for("login"))
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/download")
+@login_required
 def download():
     return send_from_directory(directory="static", path="files/cheat_sheet.pdf")
 
