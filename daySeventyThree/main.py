212fea2b9f0b9d70fe404007a8690e350b775982
Alonesa <alenakrasnov@mail.ru>
2024-03-23 21:17:20 +0000

    reformat data to pivot

--- daySeventyThree/main.py
+++ daySeventyThree/main.py
@@ -1,24 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv('QueryResults.csv', names=['DATE', 'TAG', 'POSTS'], header=0)
 pd.options.display.float_format = '{:,.2f}'.format
-
-languages = {}
-
-
-def count_posts():
-    counter = data.groupby('TAG').mean(True)
-    return counter
-
-
-popularity = count_posts()
-
-print(popularity)
-
-
-def create_unique():
-    d = data
-    unique = d['TAG'].unique()
-
-    for value in unique:
-        languages[value] = {}
+data = pd.read_csv('QueryResults.csv', names=['DATE', 'TAG', 'POSTS'], header=0)
+data.DATE = pd.to_datetime(data.DATE)
+pivoted = data.pivot(index='DATE', columns='TAG', values='POSTS')
+pivoted.fillna(0, inplace=True)
