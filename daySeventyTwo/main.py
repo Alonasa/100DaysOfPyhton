a8871ec4dd4fc07c3cc1ff47d95e5d73fb70f7a2
Alonesa <alenakrasnov@mail.ru>
2024-03-23 16:54:27 +0000

    find graduates with a highest salariess spread, and the highest salaries after the graduating and make table from that data

--- daySeventyTwo/main.py
+++ daySeventyTwo/main.py
@@ -22,3 +22,16 @@ print(
     f'will get '
     f'{wiped_df["Mid-Career 90th Percentile Salary"][max_salary_idx]}$ per year.\n10% people with 10+y.e. earn '
     f'{wiped_df["Mid-Career 10th Percentile Salary"][max_salary_idx]}$ ')
+
+wiped_df.insert(1, 'Spread', wiped_df['Mid-Career 90th Percentile Salary'].subtract(wiped_df['Mid-Career 10th '
+                                                                                             'Percentile Salary']))
+minimal_risk_salary = wiped_df['Spread'].idxmin()
+maximal_risk_salary = wiped_df['Spread'].idxmax()
+print(f'The minimal risk to become a low paid specialist have a {wiped_df["Undergraduate Major"][minimal_risk_salary]}')
+print(f'The maximal risk to become a low paid specialist have a '
+      f'{wiped_df["Undergraduate Major"][maximal_risk_salary]}')
+
+sorted_salaries = wiped_df.sort_values('Spread', ascending=False)
+salaries_spread_table = sorted_salaries[['Undergraduate Major', 'Spread']]
+highest_potential = wiped_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
+highest_potential_table = highest_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head()
