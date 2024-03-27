da73a5dbce3a87e4f64a26a37e14c087a43abca9
Alonesa <alenakrasnov@mail.ru>
2024-03-27 00:48:36 +0000

    added Barchart and render data for it

--- daySeventyFour/main.py
+++ daySeventyFour/main.py
@@ -12,15 +12,13 @@ Bootstrap(app)
 
 colors_data = pd.read_csv('data/colors.csv')
 sets_data = pd.read_csv('data/sets.csv')
+themes_data = pd.read_csv('data/themes.csv')
 unique_colors = colors_data['name'].nunique()
 transparent_colors = colors_data['is_trans']
 amt_transparent = 0
 for item in transparent_colors:
     if item == 't':
         amt_transparent += 1
-print(unique_colors)
-print(amt_transparent)
-print(sets_data.head())
 get_first_lego_release = sets_data.sort_values('year').head()
 years = sets_data.sort_values('year')['year'].drop_duplicates()
 data_by_year = sets_data[sets_data['year'] == 1965]
@@ -49,7 +47,7 @@ def show_data_by_year():
     return render_template('index.html', data=ready_data, years=years)
 
 
-@app.route('/visualize-sets')
+@app.route('/show-sets')
 def visualize_sets():
     filtered_data = sets_data[sets_data['year'].isin([int(year) for year in years])]
     themes = filtered_data.groupby('year').agg({'theme_id': pd.Series.nunique})
@@ -97,6 +95,26 @@ def visualize_complexity():
     offline.plot(figure, filename='templates/scatter-plot.html', auto_open=False)
 
     return render_template('scatter-plot.html')
+
+
+@app.route('/show-themes')
+def visualize_themes():
+    themes = themes_data
+    sets = sets_data
+    count_set = sets["theme_id"].value_counts()
+    count_set = pd.DataFrame({'id': count_set.index, 'set_count': count_set.values})
+    merged_df = pd.merge(count_set, themes, on='id')
+    scatter = go.Bar(x=merged_df.name[:10], y=merged_df.set_count[:10], name='Pieces Per '
+                                                                             'Set')
+    layout = go.Layout(
+        title='Pieces per Set',
+        xaxis=go.layout.XAxis(title='Name'),
+        yaxis=go.layout.YAxis(title='Sets')
+    )
+    figure = go.Figure(data=[scatter], layout=layout)
+    offline.plot(figure, filename='templates/barchart.html', auto_open=False)
+
+    return render_template('barchart.html')
 
 
 if __name__ == "__main__":
